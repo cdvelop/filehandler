7955404652121ac8e2d55764e4fd72f988e436e1
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2023-12-28 16:27:02 -0300

    refactor delete

--- delete.go
+++ delete.go
@@ -5,25 +5,41 @@ import (
 )
 
 func (f FileHandler) Delete(u *model.User, params ...map[string]string) (err string) {
-
 	// fmt.Println("parámetros Delete recibidos:", params)
-
 	recover_data, err := f.ReadSyncDataDB(model.ReadParams{FROM_TABLE: f.Table}, params...)
 	if err != "" {
 		return "error Delete al recuperar data anterior " + err
 	}
 	// fmt.Println("DATA RECOBRADA:", recover_data)
 
-	err = f.DeleteObjectsInDB(f.Table, params...)
-	if err != "" {
-		return err
-	}
-
 	if len(recover_data) == 0 {
 		return "el archivo ya no existe en la base de datos"
 	}
 
+	err = f.DeleteFileFromHDDandDB(recover_data...)
+	if err != "" {
+		return err
+	}
+
+	// agregar campos recover_data a params para retornarla
 	for i, data := range recover_data {
+		for k, v := range data {
+			params[i][k] = v
+		}
+	}
+	// fmt.Println("DATA params DESPUÉS DE BORRAR: ", params)
+
+	return ""
+}
+
+func (f FileHandler) DeleteFileFromHDDandDB(recover_data ...map[string]string) (err string) {
+
+	err = f.DeleteObjectsInDB(f.Table, recover_data...)
+	if err != "" {
+		return err
+	}
+
+	for _, data := range recover_data {
 
 		file_path, _ := f.BuildFilePath(data)
 
@@ -33,17 +49,8 @@ func (f FileHandler) Delete(u *model.User, params ...map[string]string) (err str
 		if err != "" {
 			return "archivo " + data[f.table.Field_name] + " fue eliminado de la db pero no del hdd " + err
 		}
-
 		// fmt.Println("FileDelete err", err)
-
-		// agregar campos a la data de entrada para retornarla
-		for k, v := range data {
-			params[i][k] = v
-		}
-
 	}
 
-	// fmt.Println("DATA RECOBRADA DESPUÉS DE BORRAR: ", recover_data)
-
-	return ""
+	return
 }
